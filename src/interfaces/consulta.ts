936bbf21b5f99263ac04518b7dff9521aceeab0c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-01 21:53:24 -0300

    Aplicando o nivelamento do front

--- src/interfaces/consulta.ts
+++ src/interfaces/consulta.ts
@@ -6,7 +6,7 @@ export interface Consulta {
   id: number;
   medico: Medico;
   paciente: Paciente;
-  dataHora: string;
+  dataHora: string; // ISO string vindo do backend (ex: "2026-05-20T09:00:00")
   valor: number;
   status: StatusConsulta;
   observacoes?: string;
